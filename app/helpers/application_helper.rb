c5bc067e89eac627ab60528bcd335d44d50fef07
Rani Manoharan <ranimanoharan@gmail.com>
2011-03-09 23:10:22 -0500

    removed a file

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def title
+    # comment
+    base_title = "Rani's Sample App"
+    
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+    
+  end
 end
